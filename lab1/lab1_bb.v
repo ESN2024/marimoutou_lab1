5460fa85817f8d40a2c347ad2e920db38c17db6e
Mourougen MRMT <97475510+MourouMRMT@users.noreply.github.com>
2024-01-09 11:38:35 +0100

    lab1 premier commit
    ajout description qsys avec bouton.
    Le chenillard sans interruptions est fonctionnel

---
+++ lab1/lab1_bb.v
@@ -0,0 +1,12 @@
+
+module lab1 (
+	clk_clk,
+	pio_0_external_connection_export,
+	reset_reset_n,
+	btn_external_connection_export);	
+
+	input		clk_clk;
+	output	[7:0]	pio_0_external_connection_export;
+	input		reset_reset_n;
+	input		btn_external_connection_export;
+endmodule
