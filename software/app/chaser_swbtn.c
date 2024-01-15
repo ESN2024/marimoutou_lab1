384e855bf5774de85710854e3a30d7fdc0965f4a
Mourougen MRMT <97475510+MourouMRMT@users.noreply.github.com>
2024-01-15 14:58:05 +0100

    commentaire

--- software/app/chaser_swbtn.c
+++ software/app/chaser_swbtn.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+#include<stdint.h>
 #include "system.h"
 #include "altera_avalon_pio_regs.h"
 #include "sys/alt_stdio.h"
@@ -5,24 +7,25 @@
 #include "sys/alt_sys_init.h"
 #include <unistd.h>
 
-int time=40000;
-int switches=0;
+uint32_t time=40000;
+uiint8_t switches=0;
+
 static void irqhandler_btn (void * context)
 {
 	alt_printf("BUTTON INTERRUPT\n");
 	
-	int data=0x01;
+	uint8_t data=0x01;
 	IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x01);
 	usleep(time);
 	
-
+	//chaser leds 7 to 0
 	while(data != 0x80)
 	{
 		data=data<<1;
 		IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,data);
 		usleep(time);	
 	}
-	
+	//chaser leds 0 to 7
 	while(data != 0x01)
 	{
 		data=data>>1;
@@ -33,24 +36,30 @@ static void irqhandler_btn (void * context)
 static void irqhandler_switch (void * context)
 {
 	alt_printf("SWITCHES INTERRUPT\n");
+	// Read Switch Value
 	switches = IORD_ALTERA_AVALON_PIO_DATA(SWITCH_BASE);
 	alt_printf("sw = %x\n",switches);
 	time=switches*10000+10000;
+	
+	// Clear Interrupt
 	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(SWITCH_BASE,0b00001111);
 }
 int main()
 {
 	alt_printf("main() started\n");
-	
+	// Enable IRQ end button capture
 	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(BTN_BASE,0b1);
 	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BTN_BASE,0b1);
 	
+	// Enable IRQ end Switches capture
 	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(SWITCH_BASE,0b00001111);
 	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(SWITCH_BASE,0b00001111);
 	
+	// Register Buttons Interrupt
 	alt_ic_isr_register(SWITCH_IRQ_INTERRUPT_CONTROLLER_ID,BTN_IRQ,irqhandler_btn,NULL,NULL);
 	alt_printf("btn interrupt created\n");
-
+	
+	// Register Switches Interrupt
 	alt_ic_isr_register(SWITCH_IRQ_INTERRUPT_CONTROLLER_ID,SWITCH_IRQ,irqhandler_switch,NULL,NULL);
 	alt_printf("switches interrupt created\n");
 
--- software/app/chaser_wbtn.c
+++ software/app/chaser_wbtn.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+#include<stdint.h>
 #include "system.h"
 #include "altera_avalon_pio_regs.h"
 #include "sys/alt_stdio.h"
@@ -9,21 +11,24 @@
 
 static void irqhandler (void * context)
 {	
-	int data=0x01;
-
+	uint8_t data=0x01;
+	
+	//chaser leds 7 to 0
 	while(data != 0x80)
 	{
 		data=data<<1;
 		IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,data);
 		usleep(60000);	
 	}
-	
+	//chaser leds 0 to 7
 	while(data != 0x01)
 	{
 		data=data>>1;
 		IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,data);
 		usleep(60000);	
 	}
+	
+	// Clear Interrupt
 	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BTN_BASE,0b1);
 }
 
@@ -31,10 +36,12 @@ int main()
 {
 	alt_printf("main() started\n");
 	
+	// Enable IRQ end button capture
 	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(BTN_BASE,0b1);
 	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BTN_BASE,0b1);
 	//alt_irq_register(BTN_IRQ, NULL,(void*)irqhandler );
 	
+	// Register Interrupt
 	alt_ic_isr_register(BTN_IRQ_INTERRUPT_CONTROLLER_ID,BTN_IRQ,irqhandler,NULL,NULL);
 	alt_printf("btn interrupt created");
 	
